780e66902a241c336ade70da216ae1057b9b08e0
s18289 <s18289@pjwstk.edu.pl>
2020-04-10 13:43:16 +0200

    commit

---
+++ tutorial6/Services/IDbService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tutorial6.Models;
+
+namespace tutorial6.Services
+{
+    public interface IDbService
+    {
+        Student GetStudentByIndex(string Index);
+    }
+}
